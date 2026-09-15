6b78a4780e8f1a9370a7278c72a6656be13a7094
naruzkurai <naruzkurai@nzk.dev>
2026-09-15 17:39:55 -0400

    sync from testing version 0.1.9152139

--- build/NZK/Core/MenuItems/MenuItems.cs
+++ build/NZK/Core/MenuItems/MenuItems.cs
@@ -29,7 +29,7 @@ public static partial class MenuItems
     if (!hasZero)
     { NZK.E.D.OK(clip, 30, 29);
       return; }
-    if (NZK.Core.MenuItems.FixAnimationClipScale(clip))
+    if (FixAnimationClipScale(clip))
     { NZK.AD.R();
       NZK.E.D.OK(clip, 29, 29); }
     else
